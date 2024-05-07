06c2506efdf79d8d4b0c3aeebd177e05e555a782
vaputra <vaputra@ncsu.edu>
2024-05-07 08:36:44 -0400

    Add SNS topic and email subscription

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # S3-Personal-Uploader
 Repo to create personal S3 backup system
+
+## Usage
+```
+# Upload specific folder
+python upload.py <FULL FOLDER PATH>
+```
--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,4 @@
+## S3 Bucket
 resource "aws_s3_bucket" "vitus_mac_data_bucket" {
   bucket = var.s3_bucket_name
 }
@@ -28,4 +29,46 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_notification" "vitus_mac_data_bucket_notification" {
+  depends_on = [aws_sns_topic_subscription.vitus_mac_data_bucket_sns_topic_subscription, aws_sns_topic.vitus_mac_data_bucket_sns_topic]
+  bucket     = aws_s3_bucket.vitus_mac_data_bucket.id
+
+  topic {
+    topic_arn = aws_sns_topic.vitus_mac_data_bucket_sns_topic.arn
+    events    = ["s3:ObjectCreated:*"]
+  }
+}
+
+## SNS Topic
+resource "aws_sns_topic" "vitus_mac_data_bucket_sns_topic" {
+  name   = "vitus_mac_data_bucket_sns_topic"
+  policy = data.aws_iam_policy_document.vitus_mac_data_bucket_sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "vitus_mac_data_bucket_sns_topic_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["s3.amazonaws.com"]
+    }
+
+    actions   = ["SNS:Publish"]
+    resources = ["arn:aws:sns:*:*:vitus_mac_data_bucket_sns_topic"]
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = [aws_s3_bucket.vitus_mac_data_bucket.arn]
+    }
+  }
+}
+
+resource "aws_sns_topic_subscription" "vitus_mac_data_bucket_sns_topic_subscription" {
+  topic_arn = aws_sns_topic.vitus_mac_data_bucket_sns_topic.arn
+  protocol  = "email"
+  endpoint  = "vitus.putra@gmail.com"
 }
